7222408e9a46cd6a36fdb1d5dc57603aff1c1a6b
Michal Fudala <mail@michalfudala.com>
2022-06-30 18:11:36 +0200

    partially working kotlin<->java type interop

--- src/main/kotlin/xyz/mf7/kotlinpoet/fun/Main.kt
+++ src/main/kotlin/xyz/mf7/kotlinpoet/fun/Main.kt
@@ -1,51 +1,77 @@
 package xyz.mf7.kotlinpoet.`fun`
 
+import com.pulumi.aws.emr.inputs.ClusterBootstrapActionArgs
+import com.pulumi.kotlin.aws.emr.*
 import com.squareup.kotlinpoet.*
 import kotlinx.serialization.json.*
+import xyz.mf7.kotlinpoet.sdk.pulumiArgsFromKotlinToJava
 import java.io.File
 import kotlin.io.path.Path
 import kotlin.io.path.absolutePathString
 
 
 fun main() {
-
-    val loadedSchema = {}::class.java.getResourceAsStream("/schema.json")!!
-
-    val schemaFromJson = Json.parseToJsonElement(
-        loadedSchema.bufferedReader().readText()
-    )
-
-    val loadedSchemaClassic = { }::class.java.getResourceAsStream("/schema-aws-classic.json")!!
-
-    val schemaFromJsonClassic = Json.parseToJsonElement(
-        loadedSchemaClassic.bufferedReader().readText()
-    )
-
-    val typesForAwsNative = Json.decodeFromJsonElement<TypeMap>(schemaFromJson.jsonObject["types"]!!)
-
-    val typesForAwsClassic = Json.decodeFromJsonElement<TypeMap>(schemaFromJsonClassic.jsonObject["types"]!!)
-
-    val functionsForAwsClassic =
-        Json.decodeFromJsonElement<FunctionsMap>(schemaFromJsonClassic.jsonObject["functions"]!!)
-
-    val destination = "/Users/mfudala/workspace/pulumi-fun/calendar-ninja/infra-pulumi/app/src/main/java"
-
-    generateTypes(typesForAwsClassic).forEach {
-        it.writeTo(File(destination))
-    }
-
-//    generateTypes(typesForAwsNative).forEach {x
-//        it.writeTo(File("/Users/mfudala/workspace/kotlin-poet-fun/generated_functions/src/main/kotlin"))
+//
+//    val loadedSchema = {}::class.java.getResourceAsStream("/schema.json")!!
+//
+//    val schemaFromJson = Json.parseToJsonElement(
+//        loadedSchema.bufferedReader().readText()
+//    )
+//
+//    val loadedSchemaClassic = { }::class.java.getResourceAsStream("/schema-aws-classic.json")!!
+//
+//    val schemaFromJsonClassic = Json.parseToJsonElement(
+//        loadedSchemaClassic.bufferedReader().readText()
+//    )
+//
+//    val typesForAwsNative = Json.decodeFromJsonElement<TypeMap>(schemaFromJson.jsonObject["types"]!!)
+//
+//    val typesForAwsClassic = Json.decodeFromJsonElement<TypeMap>(schemaFromJsonClassic.jsonObject["types"]!!)
+//
+//    val functionsForAwsClassic =
+//        Json.decodeFromJsonElement<FunctionsMap>(schemaFromJsonClassic.jsonObject["functions"]!!)
+//
+//    val destination = "/Users/mfudala/workspace/pulumi-fun/calendar-ninja/infra-pulumi/app/src/main/java"
+//
+//    generateTypes(typesForAwsClassic).forEach {
+//        it.writeTo(File(destination))
 //    }
+//
+////    generateTypes(typesForAwsNative).forEach {x
+////        it.writeTo(File("/Users/mfudala/workspace/kotlin-poet-fun/generated_functions/src/main/kotlin"))
+////    }
+//
+//
+//    generateFunctions(functionsForAwsClassic).generatedFiles.forEach {
+//        it.writeTo(File(destination))
+//    }
+//
+//    generateAndSaveCommon(destination, "com.pulumi.kotlin.aws")
+//
+//    generateAndSaveVersionAndPluginFile(File(File(destination).parent, "resources").absolutePath, "com.pulumi.kotlin.aws")
 
+    data class A(val string: String, val znt: Int)
 
-    generateFunctions(functionsForAwsClassic).generatedFiles.forEach {
-        it.writeTo(File(destination))
-    }
+    val omg = A("asd", 1)
 
-    generateAndSaveCommon(destination, "com.pulumi.kotlin.aws")
+    val kotlin = ClusterBootstrapAction(
+        listOf("1", "2"),
+        "omg",
+        "a123123asdasd"
+    )
 
-    generateAndSaveVersionAndPluginFile(File(File(destination).parent, "resources").absolutePath, "com.pulumi.kotlin.aws")
+    val omg2 = GetReleaseLabelsArgs(
+        GetReleaseLabelsFilters(
+            application = "Asd",
+            prefix = "omgomg"
+        )
+    )
+
+    val converted = pulumiArgsFromKotlinToJava(omg2, com.pulumi.aws.emr.inputs.GetReleaseLabelsArgs::class.java)
+
+//    val converted = justTesting(omg)
+
+    println(converted.filters().flatMap { it.application() })
 }
 
 fun generateAndSaveVersionAndPluginFile(baseResourcesPath: String, packageName: String) {
--- src/main/kotlin/xyz/mf7/kotlinpoet/sdk/PulumiJavaKotlinInterop.kt
+++ src/main/kotlin/xyz/mf7/kotlinpoet/sdk/PulumiJavaKotlinInterop.kt
@@ -1,9 +1,46 @@
 package xyz.mf7.kotlinpoet.sdk
-//
+
+import kotlin.reflect.KProperty
+
 //inline fun <reified FROM, reified TO> pulumiFromJavaToKotlin(from: FROM): TO {
-//
+//    return null
 //}
-//
-//inline fun <reified FROM, reified TO> pulumiFromKotlinToJava(from: FROM): TO {
-//
-//}
+
+
+fun <T1> isPulumiType(clazz: Class<T1>): Boolean {
+    return clazz.packageName.contains("pulumi")
+}
+
+
+
+fun <FROM, TO> pulumiArgsFromKotlinToJava(from: FROM & Any, to: Class<TO>): TO {
+    val kotlinProperties = from::class.members.filterIsInstance<KProperty<*>>()
+
+    val toBuilder = to.getMethod("builder").invoke(null)
+    val builderMethods = toBuilder.javaClass.methods
+        .filter { it.returnType.name.endsWith("Builder") }
+
+    val propertyNameToValue = kotlinProperties.associate { it.name to it }
+    builderMethods
+        .filterNot { it.parameterTypes.any { type -> type.name.contains("Output") } || it.isVarArgs }
+        .forEach { builderMethod ->
+        propertyNameToValue.get(builderMethod.name) ?. let {
+            val getterValue = it.getter.call(from)
+            val valueToSet = if(getterValue == null) {
+                null
+            }
+            else if(isPulumiType(getterValue.javaClass)) {
+                pulumiArgsFromKotlinToJava(getterValue, builderMethod.parameters.get(0).type)
+            }
+            else {
+                getterValue
+            }
+
+            builderMethod.invoke(toBuilder, valueToSet)
+        }
+    }
+
+    val result = toBuilder.javaClass.getMethod("build").invoke(toBuilder)
+
+    return result as TO
+}
