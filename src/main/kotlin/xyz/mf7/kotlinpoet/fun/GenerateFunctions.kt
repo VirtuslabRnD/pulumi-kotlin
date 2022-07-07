c528dd46c2c939a5cf0dd1c38a760abc521e850e
Michal Fudala <mail@michalfudala.com>
2022-07-07 18:25:32 +0200

    snapshot some partial refactoring (does not compile)

--- src/main/kotlin/xyz/mf7/kotlinpoet/fun/GenerateFunctions.kt
+++ src/main/kotlin/xyz/mf7/kotlinpoet/fun/GenerateFunctions.kt
@@ -1,10 +1,46 @@
 package xyz.mf7.kotlinpoet.`fun`
 
+import com.pulumi.core.Output
+import com.pulumi.core.TypeShape
+import com.pulumi.deployment.Deployment
+import com.pulumi.deployment.DeploymentInstance
+import com.pulumi.kotlin.PulumiJavaKotlinInterop
+import com.pulumi.resources.InvokeArgs
 import com.squareup.kotlinpoet.*
 import com.squareup.kotlinpoet.MemberName.Companion.member
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.KFunction3
 
+inline fun <reified T> classNameOf(): ClassName {
+    return T::class.asClassName()
+}
+
+inline fun <reified T: Any, R> memberOf(f: KFunction<R>): MemberName {
+    return classNameOf<T>().member(f.name)
+}
+
+fun <T: Any, R> classWithMember(c: KClass<T>, f: KFunction<R>): MemberName {
+    return c.asClassName().member(f.name)
+}
+
+fun ClassName.member(f: KFunction<Any>): MemberName {
+    return member(f.name)
+}
+
+fun <A, R> ClassName.staticMember(f: (A) -> R): MemberName {
+    return ClassName("a", "B").member(f.toString())
+}
+
+fun <O, A, B, C, R> member(f: O.(A, B, C) -> R): MemberName {
+    return ClassName("a", "B").member(f.toString())
+}
+
+fun main() {
+}
 
 fun generateMethodBody(it: FunSpec.Builder, name: String, outputType: TypeSpec): FunSpec.Builder {
+
     val deployPackage = "com.pulumi.deployment"
     val corePackage = "com.pulumi.core"
     val providerPackage = "com.pulumi.kotlin.aws" // TODO: parametrize
@@ -31,28 +67,27 @@ fun generateMethodBody(it: FunSpec.Builder, name: String, outputType: TypeSpec):
 
 
 
-    it.addStatement(
-        "val mappedArgs = %T.%M(args) as %T",
-        pulumiInterop,
-        toJava,
+    val mappedArgsBlock = CodeBlock.of(
+        "val mappedArgs = %M(args) as %T",
+        classNameOf<PulumiJavaKotlinInterop>().member(PulumiJavaKotlinInterop::toJava),
         invokeArgs
     )
 
-    it.addStatement(
-        "val result = %T.%M().%N(%S, %T.%M(%T.%M(%N::class.java)), args, %T.%M(%T.%M))",
-        deployment,
-        getInstance,
-        "invokeAsync",
-        name,
-        typeShape,
-        ofTypeShape,
-        pulumiInterop,
-        getTargetClassForFromKotlinToJava,
-        outputType,
-        utilities,
-        utilitiesWithVersion,
-        invokeOptions,
-        invokeOptionsEmpty
+    val javaResulTypeBlock = CodeBlock.of(
+        "val javaResultType = %M(%N::class.java)",
+        classNameOf<PulumiJavaKotlinInterop>().member(PulumiJavaKotlinInterop::getTargetClassForFromKotlinToJava),
+        outputType
+    )
+
+    val typeShapeBlock = CodeBlock.of(
+        "val typeShape = %M(javaResultType)",
+        classNameOf<TypeShape<*>>().staticMember<Class<String>, _>(TypeShape::of)
+    )
+
+    val invokeOptionsBlock = CodeBlock.of("val invokeOptions = %M(%M)", utilitiesWithVersion, invokeOptionsEmpty)
+
+    val resultBlock = CodeBlock.of("val result = %M().%N(%S, typeShape, args, invokeOptions)",
+        member<_, _, _, _, Output<String>>(Deployment::invoke)
     )
 
     it.addStatement("val awaitedResult = result.%M()", awaitFuture)
@@ -79,20 +114,7 @@ fun generateFunctions(functionsMap: FunctionsMap): GeneratedFunction {
                 val inputName = ClassName(packageNameForName(groupName), fileNameForName(name) + "Args")
                 val outputName = ClassName(packageNameForName(groupName), fileNameForName(name) + "Result")
 
-                val i = constructDataClass(inputName, function.inputs,
-                    {
-                        superclass(ClassName("com.pulumi.resources", "InvokeArgs"))
-                    },
-                    { name, _, isRequired ->
-                        addAnnotation(
-                            AnnotationSpec.builder(ClassName("com.pulumi.core.annotations", "Import"))
-                                .useSiteTarget(AnnotationSpec.UseSiteTarget.FIELD)
-                                .addMember("name = %S", name.value)
-                                .addMember("required = %L", isRequired)
-                                .build()
-                        )
-                    }
-                )
+                val i = constructDataClass(inputName, function.inputs)
                 val inputFile = FileSpec.builder(packageNameForName(groupName), fileNameForName(name) + "Args")
                     .addType(i)
                     .build()
