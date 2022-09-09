f2cd5e8a3708c5fe20d5980a6fdb594915a03a3a
Michal Fudala <mail@michalfudala.com>
2022-09-09 17:03:27 +0200

    replace hardcoded path with resource stream (forgot about this one)

--- src/main/kotlin/com/virtuslab/pulumikotlin/codegen/Codegen.kt
+++ src/main/kotlin/com/virtuslab/pulumikotlin/codegen/Codegen.kt
@@ -7,6 +7,7 @@ import com.virtuslab.pulumikotlin.codegen.step2intermediate.getTypeSpecs
 import com.virtuslab.pulumikotlin.codegen.step3codegen.CodeGenerator
 import com.virtuslab.pulumikotlin.codegen.step3codegen.GeneratorArguments
 import java.io.File
+import java.io.InputStream
 import kotlin.io.path.absolute
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.createTempDirectory
@@ -15,11 +16,8 @@ object Codegen {
     /**
      * Generates Pulumi SDK Kotlin for a particular schema and saves it to temporary directory (returned File)
      */
-    fun codegen(schema: File): File {
-        if (!schema.exists()) {
-            error("does not exist")
-        }
-        val parsedSchemas = Decoder.decode(schema.inputStream())
+    fun codegen(inputStreamWithSchema: InputStream): File {
+        val parsedSchemas = Decoder.decode(inputStreamWithSchema)
         val autonomousTypes = getTypeSpecs(parsedSchemas)
         val resourceTypes = getResourceSpecs(parsedSchemas)
         val functionTypes = getFunctionSpecs(parsedSchemas)
