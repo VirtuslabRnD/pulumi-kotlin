6489b03d71d0d7d8d7ec07de3309ac871ae1d004
Julia Plewa <jplewa@virtuslab.com>
2022-09-28 17:23:34 +0200

    use simplified Pulumi.run
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- examples/gcp-sample-project/src/main/kotlin/project/Main.kt
+++ examples/gcp-sample-project/src/main/kotlin/project/Main.kt
@@ -1,36 +1,33 @@
 package project
 
 import com.pulumi.Context
-import com.pulumi.Pulumi
 import com.pulumi.gcp.compute.kotlin.instanceResource
-import kotlinx.coroutines.runBlocking
+import com.pulumi.kotlin.Pulumi
 
 fun main() {
     Pulumi.run { ctx: Context ->
-        runBlocking {
-            val instance = instanceResource("gcp-sample-project") {
-                args {
-                    machineType("e2-micro")
-                    zone("europe-central2-a")
-                    tags("foo", "bar")
-                    bootDisk {
-                        initializeParams {
-                            image("debian-cloud/debian-11")
-                        }
-                    }
-                    networkInterfaces(
-                        {
-                            network("default")
-                        },
-                    )
-                    metadata(mapOf("foo" to "bar"))
-                    metadataStartupScript("echo hi > /test.txt")
-                    serviceAccount {
-                        scopes("cloud-platform")
+        val instance = instanceResource("gcp-sample-project") {
+            args {
+                machineType("e2-micro")
+                zone("europe-central2-a")
+                tags("foo", "bar")
+                bootDisk {
+                    initializeParams {
+                        image("debian-cloud/debian-11")
                     }
                 }
+                networkInterfaces(
+                    {
+                        network("default")
+                    },
+                )
+                metadata(mapOf("foo" to "bar"))
+                metadataStartupScript("echo hi > /test.txt")
+                serviceAccount {
+                    scopes("cloud-platform")
+                }
             }
-            ctx.export("instanceName", instance.name)
         }
+        ctx.export("instanceName", instance.name)
     }
 }
