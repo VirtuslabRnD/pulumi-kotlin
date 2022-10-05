f6774939b44b90fa05dbd03391d96d6f9edc590e
Julia Plewa <jplewa@virtuslab.com>
2022-10-05 10:45:08 +0200

    update library versions
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,17 +17,17 @@ repositories {
 }
 
 dependencies {
-    implementation("com.pulumi:pulumi:(,1.0]")
+    implementation("com.pulumi:pulumi:0.6.0")
 
     implementation("com.squareup:kotlinpoet:1.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
 
     implementation("com.github.ajalt.clikt:clikt:3.5.0")
 
     implementation("com.squareup.tools.build:maven-archeologist:0.0.10")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.9")
     testImplementation(kotlin("test"))
     testImplementation("com.google.cloud:google-cloud-compute:1.12.1")
