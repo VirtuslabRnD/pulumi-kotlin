bb0fc04c66fccecbbac926d4a4b2d2a44687f150
Julia Plewa <jplewa@virtuslab.com>
2022-10-17 16:12:27 +0200

    refactor Decoder
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- src/main/kotlin/com/virtuslab/pulumikotlin/codegen/step1schemaparse/Decoder.kt
+++ src/main/kotlin/com/virtuslab/pulumikotlin/codegen/step1schemaparse/Decoder.kt
@@ -1,6 +1,7 @@
 package com.virtuslab.pulumikotlin.codegen.step1schemaparse
 
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonObject
 import java.io.InputStream
@@ -9,16 +10,15 @@ object Decoder {
     fun decode(inputStream: InputStream): ParsedSchema {
         val schema = Json.parseToJsonElement(inputStream.bufferedReader().readText())
 
-        val types = schema.jsonObject["types"]
-            ?.let { Json.decodeFromJsonElement<TypesMap>(it) }
-            .orEmpty()
-        val functions = schema.jsonObject["functions"]
-            ?.let { Json.decodeFromJsonElement<FunctionsMap>(it) }
-            .orEmpty()
-        val resources = schema.jsonObject["resources"]
-            ?.let { Json.decodeFromJsonElement<ResourcesMap>(it) }
-            .orEmpty()
+        val types = schema.decodeOne<String, SchemaModel.RootTypeProperty>("types")
+        val functions = schema.decodeOne<String, SchemaModel.Function>("functions")
+        val resources = schema.decodeOne<String, SchemaModel.Resource>("resources")
 
         return ParsedSchema(types, functions, resources)
     }
+
+    private inline fun <reified K, reified V> JsonElement.decodeOne(key: String): Map<K, V> =
+        jsonObject[key]
+            ?.let { Json.decodeFromJsonElement<Map<K, V>>(it) }
+            .orEmpty()
 }
