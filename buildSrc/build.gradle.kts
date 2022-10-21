762e341c8b2548f699b8bdc7d19488117b7ca0d3
Julia Plewa <jplewa@virtuslab.com>
2022-10-21 09:57:30 +0200

    add script for tagging
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -28,10 +28,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-maven-serialization:1.7.20")
     implementation("org.apache.maven:maven-artifact:3.8.6")
     implementation("log4j:log4j:1.2.14")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
 
-    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.20")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-    testImplementation("io.ktor:ktor-client-mock:2.1.2")
 }
 
 tasks.test {
