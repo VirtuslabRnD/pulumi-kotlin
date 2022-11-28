a4191a5eb9099afa13dd0ff4cf3a1049b92c9c72
Julia Plewa <jplewa@virtuslab.com>
2022-11-28 15:24:08 +0100

    update error message
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- src/main/kotlin/com/virtuslab/pulumikotlin/codegen/sdk/PulumiNullFieldException.kt
+++ src/main/kotlin/com/virtuslab/pulumikotlin/codegen/sdk/PulumiNullFieldException.kt
@@ -3,4 +3,6 @@
 
 package com.pulumi.kotlin
 
-class PulumiNullFieldException(name: String) : RuntimeException("Field $name is null")
+class PulumiNullFieldException(name: String) : RuntimeException(
+    "Field $name is required but was not set (or was set to null)",
+)
