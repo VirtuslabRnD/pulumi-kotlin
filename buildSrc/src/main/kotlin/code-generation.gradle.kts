f444e8eede502dac2412f17e525948399b390de9
Julia Plewa <jplewa@virtuslab.com>
2022-12-02 16:12:27 +0100

    add default dependencies

--- buildSrc/src/main/kotlin/code-generation.gradle.kts
+++ buildSrc/src/main/kotlin/code-generation.gradle.kts
@@ -16,6 +16,8 @@ val tasksToDisable: List<(String) -> String> = listOf(
 
 val commonDependencies = listOf(
     "org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.4.1",
+    "org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4",
+    "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4",
 )
 
 val createTasksForProvider by extra {
