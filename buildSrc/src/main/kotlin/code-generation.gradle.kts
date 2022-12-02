ee7ad8b0a30d6f9acf344882e271e932bc4ef68d
Julia Plewa <34098778+jplewa@users.noreply.github.com>
2022-12-02 17:02:18 +0100

    Add default dependencies (#166)
    ## Task
    
    Resolves: https://github.com/VirtuslabRnD/pulumi-kotlin/issues/157
    
    ## Description
    
    I added an additional custom dependency to our libraries
    (`org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4`).
    
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- buildSrc/src/main/kotlin/code-generation.gradle.kts
+++ buildSrc/src/main/kotlin/code-generation.gradle.kts
@@ -16,6 +16,7 @@ val tasksToDisable: List<(String) -> String> = listOf(
 
 val commonDependencies = listOf(
     "org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.4.1",
+    "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4",
 )
 
 val createTasksForProvider by extra {
