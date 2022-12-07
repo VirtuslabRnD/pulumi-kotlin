79a17c872e80effbeaf350795b17e6d785a67c9c
Julia Plewa <jplewa@virtuslab.com>
2022-12-07 14:31:07 +0100

    add periods back to old test cases
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- src/test/kotlin/com/virtuslab/pulumikotlin/codegen/step3codegen/KDocGeneratorTest.kt
+++ src/test/kotlin/com/virtuslab/pulumikotlin/codegen/step3codegen/KDocGeneratorTest.kt
@@ -18,10 +18,10 @@ internal class KDocGeneratorTest {
     @Test
     fun `does not add line breaks to very long lines`() {
         val description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
-            "incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam, quis nostrud exercitation " +
-            "ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit " +
-            "in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat " +
-            "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+            "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation " +
+            "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " +
+            "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat " +
+            "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         val className = "LongLines"
 
         assertKDocContentEquals(
@@ -129,7 +129,7 @@ internal class KDocGeneratorTest {
     fun `removes examples tags`() {
         val description =
             """{{% examples %}}
-              |There are some examples here
+              |There are some examples here.
               |{{% example %}}
               |{{% /example %}}
               |{{% /examples %}}"""
@@ -140,7 +140,7 @@ internal class KDocGeneratorTest {
             className,
             description,
             """/**
-              | * There are some examples here
+              | * There are some examples here.
               | */"""
                 .trimMargin(),
         )
