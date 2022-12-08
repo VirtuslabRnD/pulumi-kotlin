c9273b2d9ab8f47afa117c0d7e1c935dd4cb1356
Julia Plewa <jplewa@virtuslab.com>
2022-12-08 11:08:59 +0100

    add alicloud provider
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- src/main/kotlin/com/virtuslab/pulumikotlin/codegen/utils/Constants.kt
+++ src/main/kotlin/com/virtuslab/pulumikotlin/codegen/utils/Constants.kt
@@ -6,5 +6,7 @@ object Constants {
 
     val DUPLICATED_TYPES = mapOf(
         "azure-native:network:IpAllocationMethod" to "azure-native:network:IPAllocationMethod",
+        "alicloud:alb/ListenerXforwardedForConfig:ListenerXforwardedForConfig" to
+            "alicloud:alb/ListenerXForwardedForConfig:ListenerXForwardedForConfig",
     )
 }
