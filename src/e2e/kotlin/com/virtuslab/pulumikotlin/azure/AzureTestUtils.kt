987c49d3b86314c954e076c1226ee49fcf047f0e
Michal Fudala <mail@michalfudala.com>
2022-12-16 17:46:32 +0100

    use client id and client secret when authenticating through SDK

--- src/e2e/kotlin/com/virtuslab/pulumikotlin/azure/AzureTestUtils.kt
+++ src/e2e/kotlin/com/virtuslab/pulumikotlin/azure/AzureTestUtils.kt
@@ -2,7 +2,7 @@ package com.virtuslab.pulumikotlin.azure
 
 import com.azure.core.management.AzureEnvironment.AZURE
 import com.azure.core.management.profile.AzureProfile
-import com.azure.identity.DefaultAzureCredentialBuilder
+import com.azure.identity.ClientSecretCredentialBuilder
 import com.azure.resourcemanager.compute.ComputeManager
 import com.azure.resourcemanager.compute.models.VirtualMachine
 import com.azure.resourcemanager.compute.models.VirtualMachineSizeTypes
@@ -11,10 +11,17 @@ import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 fun getVirtualMachine(virtualMachineId: String): VirtualMachine {
+    val clientId = System.getenv("ARM_CLIENT_ID")
+    val clientSecret = System.getenv("ARM_CLIENT_SECRET")
     val subscriptionId = System.getenv("ARM_SUBSCRIPTION_ID")
     val tenantId = System.getenv("ARM_TENANT_ID")
+
     val computeManager = ComputeManager.authenticate(
-        DefaultAzureCredentialBuilder().build(),
+        ClientSecretCredentialBuilder()
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .tenantId(tenantId)
+            .build(),
         AzureProfile(tenantId, subscriptionId, AZURE),
     )
 
