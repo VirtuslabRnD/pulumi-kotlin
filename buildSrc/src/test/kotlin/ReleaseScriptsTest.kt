28f249b0690ba16d935be7ff0f3a2e6c0e4c59df
Michal Fudala <mail@michalfudala.com>
2023-01-18 21:48:40 +0700

    Improve README.md after feedback in #199 (#201)
    ## Task
    
    Resolves: #151 
    
    ## Description
    
    Improvements after feedback in
    https://github.com/VirtuslabRnD/pulumi-kotlin/pull/199.
    
    - consistent usage of snake case in settings.xml example
    - warn that docs only include Java SDK example snippets
    - convert long inline links into reference links
    
    Merge only after https://github.com/VirtuslabRnD/jvm-lab/pull/82 is
    merged (so that workflows running after merges to `main` are green).

--- buildSrc/src/test/kotlin/ReleaseScriptsTest.kt
+++ buildSrc/src/test/kotlin/ReleaseScriptsTest.kt
@@ -2,6 +2,7 @@ import org.apache.commons.lang3.RandomStringUtils
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Disabled
 import org.semver4j.Semver
 import java.io.File
 import java.nio.file.Files
@@ -90,6 +91,7 @@ class ReleaseScriptsTest {
     }
 
     @Test
+    @Disabled("Disabled due to unreliable search.maven.org (https://github.com/VirtuslabRnD/pulumi-kotlin/issues/202)")
     fun `updates provider schema versions`() {
         val temporaryGitRepository = File("build/tmp/provider-update-test-${RandomStringUtils.randomAlphanumeric(10)}")
         val beforeUpdateFileName = "before-schema-update.json"
