56cacb130ebcf83616ca3a3e4ebc7ec0e38aa920
Julia Plewa <34098778+jplewa@users.noreply.github.com>
2023-02-17 13:11:47 +0100

    Create release (#210)
    ## Task
    
    Related to: https://github.com/VirtuslabRnD/pulumi-kotlin/issues/204
    
    ## Description
    
    This release includes the following versions:
    cloudflare/v4.15.0.0
    slack/v0.3.1.0
    github/v5.4.0.0
    random/v4.11.2.0
    gcp/v6.49.0.0
    google-native/v0.28.0.0
    aws/v5.29.1.0
    aws-native/v0.47.0.0
    azure/v5.35.0.0
    azure-native/v1.93.0.0
    kubernetes/v3.24.2.0
    nomad/v0.4.0
    docker/v3.6.1.0
    gitlab/v4.9.0.1
    digitalocean/v4.18.0.0
    alicloud/v3.31.0.0
    
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,28 +13,28 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-val ktorVersion by extra { "2.1.3" }
+val ktorVersion by extra { "2.2.3" }
 
 dependencies {
     implementation(kotlin("gradle-plugin", version = "1.7.20"))
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
-    implementation("de.undercouch.download:de.undercouch.download.gradle.plugin:5.3.0")
+    implementation("de.undercouch.download:de.undercouch.download.gradle.plugin:5.3.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation(kotlin("stdlib"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-html:0.8.0")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation(kotlin("maven-serialization"))
-    implementation("org.apache.maven:maven-artifact:3.8.6")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
-    implementation("org.semver4j:semver4j:2.2.0")
+    implementation("org.apache.maven:maven-artifact:3.9.0")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.4.0.202211300538-r")
+    implementation("org.semver4j:semver4j:4.2.0")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.test {
