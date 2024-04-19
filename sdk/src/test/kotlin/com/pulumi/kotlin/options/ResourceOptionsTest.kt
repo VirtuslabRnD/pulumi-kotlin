07bc47aa02daaea2a8015e04ac5fbf2c5f197b3a
Julia Plewa <jplewa@virtuslab.com>
2024-04-19 22:19:35 +0200

    add Detekt suppression
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- sdk/src/test/kotlin/com/pulumi/kotlin/options/ResourceOptionsTest.kt
+++ sdk/src/test/kotlin/com/pulumi/kotlin/options/ResourceOptionsTest.kt
@@ -33,6 +33,7 @@ import com.pulumi.resources.ResourceOptions as JavaResourceOptions
 import com.pulumi.resources.ResourceTransformation.Args as JavaResourceTransformationArgs
 import com.pulumi.resources.ResourceTransformation.Result as JavaResourceTransformationResult
 
+@Suppress("UnusedPrivateMember")
 internal class ResourceOptionsTest {
 
     @BeforeEach
