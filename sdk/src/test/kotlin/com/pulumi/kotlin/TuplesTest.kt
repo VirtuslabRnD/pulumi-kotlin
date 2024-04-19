d37100fc0ab2e13533fd35b8334cfeb8d85a5809
Julia Plewa <jplewa@virtuslab.com>
2024-04-19 22:31:31 +0200

    update tests
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- sdk/src/test/kotlin/com/pulumi/kotlin/TuplesTest.kt
+++ sdk/src/test/kotlin/com/pulumi/kotlin/TuplesTest.kt
@@ -1,6 +1,8 @@
 package com.pulumi.kotlin
 
 import com.pulumi.core.Tuples
+import java.math.BigDecimal
+import java.math.BigInteger
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -8,85 +10,85 @@ class TuplesTest {
 
     @Test
     fun `can destruct Tuple1`() {
-        val tuple = Tuples.Tuple1("one")
+        val tuple = Tuples.Tuple1(1.toByte())
         val (value1) = tuple
-        assertEquals("one", value1)
+        assertEquals(1.toByte(), value1)
     }
 
     @Test
     fun `can destruct Tuple2`() {
-        val tuple = Tuples.Tuple2("one", "two")
+        val tuple = Tuples.Tuple2(1.toByte(), 2)
         val (value1, value2) = tuple
-        assertEquals("one", value1)
-        assertEquals("two", value2)
+        assertEquals(1.toByte(), value1)
+        assertEquals(2, value2)
     }
 
     @Test
     fun `can destruct Tuple3`() {
-        val tuple = Tuples.Tuple3("one", "two", "three")
+        val tuple = Tuples.Tuple3(1.toByte(), 2, 3L)
         val (value1, value2, value3) = tuple
-        assertEquals("one", value1)
-        assertEquals("two", value2)
-        assertEquals("three", value3)
+        assertEquals(1.toByte(), value1)
+        assertEquals(2, value2)
+        assertEquals(3L, value3)
     }
 
     @Test
     fun `can destruct Tuple4`() {
-        val tuple = Tuples.Tuple4("one", "two", "three", 4L)
+        val tuple = Tuples.Tuple4(1.toByte(), 2, 3L, 4.0f)
         val (value1, value2, value3, value4) = tuple
-        assertEquals("one", value1)
-        assertEquals("two", value2)
-        assertEquals("three", value3)
-        assertEquals(4L, value4)
+        assertEquals(1.toByte(), value1)
+        assertEquals(2, value2)
+        assertEquals(3L, value3)
+        assertEquals(4.0f, value4)
     }
 
     @Test
     fun `can destruct Tuple5`() {
-        val tuple = Tuples.Tuple5("one", "two", "three", 4L, 5)
+        val tuple = Tuples.Tuple5(1.toByte(), 2, 3L, 4.0f, 5.0)
         val (value1, value2, value3, value4, value5) = tuple
-        assertEquals("one", value1)
-        assertEquals("two", value2)
-        assertEquals("three", value3)
-        assertEquals(4L, value4)
-        assertEquals(5, value5)
+        assertEquals(1.toByte(), value1)
+        assertEquals(2, value2)
+        assertEquals(3L, value3)
+        assertEquals(4.0f, value4)
+        assertEquals(5.0, value5)
     }
 
     @Test
     fun `can destruct Tuple6`() {
-        val tuple = Tuples.Tuple6("one", "two", "three", 4L, 5, null as Int?)
+        val tuple = Tuples.Tuple6(1.toByte(), 2, 3L, 4.0f, 5.0, BigInteger.valueOf(6))
         val (value1, value2, value3, value4, value5, value6) = tuple
-        assertEquals("one", value1)
-        assertEquals("two", value2)
-        assertEquals("three", value3)
-        assertEquals(4L, value4)
-        assertEquals(5, value5)
-        assertEquals(null, value6)
+        assertEquals(1.toByte(), value1)
+        assertEquals(2, value2)
+        assertEquals(3L, value3)
+        assertEquals(4.0f, value4)
+        assertEquals(5.0, value5)
+        assertEquals(BigInteger.valueOf(6), value6)
     }
 
     @Test
     fun `can destruct Tuple7`() {
-        val tuple = Tuples.Tuple7("one", "two", "three", 4L, 5, null as Int?, Tuples.Tuple1("seven"))
+        val tuple = Tuples.Tuple7(1.toByte(), 2, 3L, 4.0f, 5.0, BigInteger.valueOf(6), Tuples.Tuple1("seven"))
         val (value1, value2, value3, value4, value5, value6, value7) = tuple
-        assertEquals("one", value1)
-        assertEquals("two", value2)
-        assertEquals("three", value3)
-        assertEquals(4L, value4)
-        assertEquals(5, value5)
-        assertEquals(null, value6)
+        assertEquals(1.toByte(), value1)
+        assertEquals(2, value2)
+        assertEquals(3L, value3)
+        assertEquals(4.0f, value4)
+        assertEquals(5.0, value5)
+        assertEquals(BigInteger.valueOf(6), value6)
         assertEquals(Tuples.Tuple1("seven"), value7)
     }
 
     @Test
     fun `can destruct Tuple8`() {
-        val tuple = Tuples.Tuple8("one", "two", "three", 4L, 5, null as Int?, Tuples.Tuple1("seven"), 8.0)
+        val tuple = Tuples.Tuple8(1.toByte(), 2, 3L, 4.0f, 5.0, BigInteger.valueOf(6), Tuples.Tuple1("seven"), null as BigDecimal?)
         val (value1, value2, value3, value4, value5, value6, value7, value8) = tuple
-        assertEquals("one", value1)
-        assertEquals("two", value2)
-        assertEquals("three", value3)
-        assertEquals(4L, value4)
-        assertEquals(5, value5)
-        assertEquals(null, value6)
+        assertEquals(1.toByte(), value1)
+        assertEquals(2, value2)
+        assertEquals(3L, value3)
+        assertEquals(4.0f, value4)
+        assertEquals(5.0, value5)
+        assertEquals(BigInteger.valueOf(6), value6)
         assertEquals(Tuples.Tuple1("seven"), value7)
-        assertEquals(8.0, value8)
+        assertEquals(null, value8)
     }
 }
