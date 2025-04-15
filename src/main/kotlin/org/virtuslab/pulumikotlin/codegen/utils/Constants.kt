799e184ebc31f886ff89e0d0fca6cb07e286f642
Julia Plewa <jplewa@virtuslab.com>
2025-04-16 00:05:04 +0200

    add exclusion for `StorageAutogrow` key
    Signed-off-by: Julia Plewa <jplewa@virtuslab.com>

--- src/main/kotlin/org/virtuslab/pulumikotlin/codegen/utils/Constants.kt
+++ src/main/kotlin/org/virtuslab/pulumikotlin/codegen/utils/Constants.kt
@@ -8,6 +8,7 @@ object Constants {
         "azure-native:network:IpAllocationMethod" to "azure-native:network:IPAllocationMethod",
         "alicloud:alb/ListenerXforwardedForConfig:ListenerXforwardedForConfig" to
             "alicloud:alb/ListenerXForwardedForConfig:ListenerXForwardedForConfig",
+        "azure-native:dbforpostgresql:StorageAutogrow" to "azure-native:dbforpostgresql:StorageAutoGrow",
         // note: this is wrong, but it's what is generated in Java
         // see: https://github.com/pulumi/pulumi-azure-native/issues/4107
         "azure-native:apimanagement:LlmDiagnosticSettings" to "azure-native:apimanagement:LLMDiagnosticSettings",
